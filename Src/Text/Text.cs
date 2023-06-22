aec95cf779c467a6ef693b27ed6efbeb3713a5cf
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 07:16:52 +0300

    add getter and setter

---
+++ Src/Text/Text.cs
@@ -0,0 +1,20 @@
+namespace OOPAdventure;
+
+public static class Text
+{
+    private static Language _language;
+    public static Language Language
+    {
+        get
+        {
+            if (_language == null)
+                throw new Exception("No language loaded.");
+            return _language;
+        }
+    }
+
+    public static void LoadLanguage(Language language)
+    {
+        _language = language;
+    }
+}
