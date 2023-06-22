7b0ee7d83d01819a3e13f3622b29c18b05524c00
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 11:11:18 +0300

    add rooms related properties and methods in House class

--- Src/Map/House.Move.cs
+++ Src/Map/House.Move.cs
@@ -1,13 +1,17 @@
 namespace OOPAdventure;
 
-public partial class House {
+public partial class House
+{
+    public Room CurrentRoom { get; set; }
+
     public void GoToRoom(int index)
     {
-
+        if (CurrentRoom != null)
+            CurrentRoom.Visited = true;
+        CurrentRoom = Rooms[index];
     }
 
-    public void GoToStartingRoom()
-    {
-        
+    public void GoToStartingRoom() { 
+        GoToRoom(_rnd.Next(0, Rooms.Length));
     }
 }
--- Src/Map/House.cs
+++ Src/Map/House.cs
@@ -4,6 +4,10 @@ public partial class House
 {
     public Player Player { get; }
     private readonly Random _rnd = new(1234);
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public Room[] Rooms { get; private set; }
+
     public House(Player player)
     {
         Player = player;
