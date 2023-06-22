5aaa2fb06d736ab92e668da0f4fdd76b20c66942
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 07:15:52 +0300

    set the values for properties inherited from Language abstract class

--- Src/Text/English.cs
+++ Src/Text/English.cs
@@ -5,5 +5,7 @@ public class English : Language
     public English()
     {
         ChooseYourName = "Hello, what is your name?";
+        DefaultName = "No Name";
+        Welcome = "Welcome {0} to your OOP adventure";
     }
 }
