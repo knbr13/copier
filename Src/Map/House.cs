5374a8530fbbc5e8bdee3ac933e116c630ec98e0
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 07:36:23 +0300

    create House partial class

---
+++ Src/Map/House.cs
@@ -0,0 +1,11 @@
+namespace OOPAdventure;
+
+public partial class House
+{
+    public Player Player { get; }
+    private readonly Random _rnd = new(1234);
+    public House(Player player)
+    {
+        Player = player;
+    }
+}
