6e1607d56e6e7aef689d977127e176cc5d37a0b1
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 11:00:50 +0300

    create Room class

--- Src/Text/English.cs
+++ Src/Text/English.cs
@@ -7,5 +7,7 @@ public class English : Language
         ChooseYourName = "Hello, what is your name?";
         DefaultName = "No Name";
         Welcome = "Welcome {0} to your OOP adventure";
+        DefaultRoomName = "Room {0} ({1},{2})";
+        DefaultRoomDescription = "You are in a room with doors to the {0}.";
     }
 }
