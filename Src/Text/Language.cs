ebfa74bc5a0ddfd9ffc82eba272dbf68bf7ea55b
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 11:01:53 +0300

    add 2 more properties

--- Src/Text/Language.cs
+++ Src/Text/Language.cs
@@ -5,4 +5,6 @@ public abstract class Language
     public string Welcome { get; protected set; } = "";
     public string ChooseYourName { get; protected set; } = "";
     public string DefaultName { get; protected set; } = "";
+    public string DefaultRoomName { get; protected set; } = "";
+    public string DefaultRoomDescription { get; protected set; } = "";
 }
