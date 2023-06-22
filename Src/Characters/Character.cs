2c32eee4c314664ebb472b3001973f5435596f87
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 06:53:09 +0300

    move Character.cs and Player.cs to Characters folder

---
+++ Src/Characters/Character.cs
@@ -0,0 +1,11 @@
+namespace OOPAdventure;
+
+public abstract class Character
+{
+    public string Name { get; set; }
+
+    public Character(string name)
+    {
+        Name = name;
+    }
+}
---
+++ Src/Characters/Player.cs
@@ -0,0 +1,7 @@
+namespace OOPAdventure;
+
+public class Player : Character
+{
+    public Player(string name)
+        : base(name) { }
+}
