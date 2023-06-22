218f21dcee1dc086f8025274eef4dd5b0adc8bc5
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-06-22 06:37:53 +0300

    ask the user to enter his name

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, what is your name?");
+
+var name = Console.ReadLine();
+
+if (name == string.Empty)
+    name = "No Name";
+
+Console.WriteLine("Welcome {0} to your OOP adventure", name);
