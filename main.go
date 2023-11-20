41964d538de96ba7cadfa0ca18954b23d46bb06c
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-11-20 03:13:40 +0200

    add printCell function

--- main.go
+++ main.go
@@ -1,6 +1,21 @@
 package main
 
+import (
+	"fmt"
+)
+
 var board [3][3]rune
 var currentPlayer rune
 
 const PADDING = "   "
+
+func printCell(cellValue string) {
+	switch cellValue {
+	case "X":
+		fmt.Print(" X ")
+	case "O":
+		fmt.Print(" O ")
+	default:
+		fmt.Print("   ")
+	}
+}
